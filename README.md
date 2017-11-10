5425886b96acff4a033e5e6344999c420f94dbfc
Greg Hubert <greg.hubert@gmail.com>
2017-11-10 16:38:10 -0700

    added totalChars to output

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 This is where my work for the Java Badge will be placed.
 - StringOutOfMemory is the code to test the how many strings will result in an Out of Memory exception
+  - set Heap Size to 100m (-Xmx100m) 
 
 - To be continued
--- apprentice/src/StringOutOfMemory.java
+++ apprentice/src/StringOutOfMemory.java
@@ -5,15 +5,16 @@ public class StringOutOfMemory {
   public static void main(String[] args) {
     String s = "01234567890123456789012345678901234567890123456789";
     List<String> list = new ArrayList<>();
-    long items = 0;
+    long count = 0;
+    long totalChars = 0;
     try {
       while(true) {
-        items++;
+        count++;
         list.add(new StringBuilder(s).toString());
       }
     }
     catch (OutOfMemoryError e) {
-      System.out.println("items added: " + items);
+      System.out.println("count: " + count + " totalChars: " + list.size() * s.length());
     }
   }
 }
